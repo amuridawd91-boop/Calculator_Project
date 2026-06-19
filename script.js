d0f3abb73b4295119b3562fe62ad4357cb4fa783
I AM BATMAN <250103254+amuridawd91-boop@users.noreply.github.com>
2026-06-19 06:08:55 +0300

    added the keyboard support which was extra credit but oh well

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const display = document.querySelector("#display");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 const equalsButton = document.querySelector("#equals");
-const clearButton = document.querySelector("#clear")
+const clearButton = document.querySelector("#clear");
+const decimalButton = document.querySelector("#decimal");
+const deleteButton = document.querySelector("#backspace")
 let firstNumber = "";
 let secondNumber = "";
 let operator = ""; 
 let resultDisplayed = false;
+
 
 operatorButtons.forEach((button) => {
         button.addEventListener("click", () => {
@@ -24,11 +27,11 @@ operatorButtons.forEach((button) => {
             }
             operator = button.textContent;
         })
-    });
+});
     
 
 
- numberButtons.forEach((button) => {
+numberButtons.forEach((button) => {
         button.addEventListener("click", () =>{
             if (resultDisplayed && operator === ""){
                 firstNumber = "";
@@ -45,52 +48,51 @@ operatorButtons.forEach((button) => {
             }
           
         })
-    })
+})
 
-     function operate(operator, num1, num2) {
+    function operate(operator, num1, num2) {
 
-    if (operator === "+") {
-        return add(num1, num2);
+        if (operator === "+") {
+            return add(num1, num2);
+        }
+
+        if (operator === "-") {
+            return subtract(num1, num2);
+        }
+
+        if (operator === "*") {
+            return multiply(num1, num2);
+        }
+
+        if (operator === "/") {
+            return divide(num1, num2);
+        }
     }
-
-    if (operator === "-") {
-        return subtract(num1, num2);
-    }
-
-    if (operator === "*") {
-        return multiply(num1, num2);
-    }
-
-    if (operator === "/") {
-        return divide(num1, num2);
-    }
-}
-
 
 
     function add(a, b) {
               return a + b;
-        }
+            }
 
     function subtract(a, b) {
               return a - b;
-        }
+            }
 
     function multiply(a, b) {
                 return a * b;
-        }   
+            }   
 
     function divide(a, b) {
             if (b === 0) return "Nice try 😏";
-            return a / b;
-        }
+                return a / b;
+            }
 
     equalsButton.addEventListener("click", () =>{
         let result = operate(
                 operator, 
                 Number(firstNumber),
                 Number(secondNumber));
-            if (typeof result === "number"){
+        if (typeof result === "number"){
                 result = Math.round(result * 1000) / 1000;
             }
         display.textContent = result
@@ -98,7 +100,7 @@ operatorButtons.forEach((button) => {
         secondNumber= "";
         operator = "";  
         resultDisplayed = true
-        })
+    })
 
     clearButton.addEventListener("click", () =>{
         firstNumber = "";
@@ -106,4 +108,72 @@ operatorButtons.forEach((button) => {
         operator = "";
         resultDisplayed = false;
         display.textContent = "0"
+    })
+
+    decimalButton.addEventListener("click", () =>{
+        if (operator === ""){
+
+            if (firstNumber === "") {
+                    firstNumber = "0.";
+            } else if (!firstNumber.includes(".")){
+                firstNumber += ".";
+            }
+                display.textContent = firstNumber;
+
+        } else {
+            if (secondNumber === ""){
+                secondNumber = "0.";                    
+            }  else if (!secondNumber.includes(".")){
+                secondNumber += ".";
+            }
+            display.textContent = secondNumber;
+        }
+    })
+
+ deleteButton.addEventListener("click", () =>{
+        if (secondNumber !== ""){
+            secondNumber = secondNumber.slice(0, -1);
+            display.textContent = secondNumber || "0";
+
+        } else if (operator !== ""){
+            operator = "";
+        
+        } else {
+            firstNumber = firstNumber.slice(0, -1);
+            display.textContent = firstNumber || "0";
+        }
+ })
+
+    document.addEventListener("keydown", (event) => {
+        if (event.key >= "0" && event.key <= "9"){
+            numberButtons.forEach((button) => {
+                if (button.textContent === event.key){
+                    button.click();
+                }
+            })
+        }
+
+        if (event.key === "+" ||
+            event.key === "-" ||
+            event.key === "*" ||
+            event.key === "/" ) {
+
+                operatorButtons.forEach((button) => {
+                    if (button.textContent === event.key){
+                        button.click();
+                    }
+                });
+        }
+        if (event.key === "."){
+            decimalButton.click();
+        } 
+        if (event.key === "Enter"){
+            equalsButton.click();
+        }
+        if (event.key === "Backspace"){
+            deleteButton.click();
+        }
+        if (event.key === "Escape"){
+            clearButton.click();
+        }
     })
